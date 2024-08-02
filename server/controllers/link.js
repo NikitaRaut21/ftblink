067f5a6ddd9fc1f72fda14511d1c1f0a9529e705
NikitaRaut21 <rautn7350@gmail.com>
2024-08-02 15:19:40 +0530

    initial commit

---
+++ server/controllers/link.js
@@ -0,0 +1,33 @@
+import Link from "./../models/Link.js"
+
+const PostLink=async(req,res)=>{
+    const {target,slug,title}=req.body;
+
+    const link = new Link({
+     target,
+     slug,
+     title
+
+    })
+    const SavedLink = await link.save();
+    res.json({
+        success:true,
+        message :`link created successfully...`,
+        data:SavedLink
+    })
+}
+const  getSlugRedirect =async(req,res)=>{
+const {slug} =req.params
+const link= await Link.findOne({slug});
+if(!link){
+     return res.json({
+        success:false,
+        message:"Link not found"
+        })
+}
+link.views=link.views+1;
+await link.save();
+res.redirect(link.target)
+}
+
+export{PostLink,getSlugRedirect };
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+import { PostLink,getSlugRedirect } from './controllers/link.js';
+ 
+const  app = express();
+app.use(express.json());
+app.use(cors());
+//connect mongodb
+
+const connectDB = async()=>{
+    const conn =await mongoose.connect(process.env.MONGO_URI)
+    if(conn){
+        console.log(`mongodb connected successfully✅`)
+    }
+}
+connectDB();
+
+app.get("/health",(req,res)=>{
+    res.json({
+      success:true,
+      message:"welcome to ftb link"
+    })
+})
+
+app.post("/link", PostLink)
+
+app.get("/:slug",getSlugRedirect)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`server is running or port ${PORT}`)
+})
